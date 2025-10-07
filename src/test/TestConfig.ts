7144b0372fac2b3ed55a39ab8f2f19f3948ea861
CalebGerman <caleb.german@bentley.com>
2025-10-07 12:46:58 -0500

    Disabled lint issue

--- src/test/TestConfig.ts
+++ src/test/TestConfig.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+/* eslint-disable no-console */
 import type { AccessToken } from "@itwin/core-bentley";
 import type { TestUserCredentials } from "@itwin/oidc-signin-tool/lib/cjs/frontend";
 import { TestUsers, TestUtility } from "@itwin/oidc-signin-tool";
